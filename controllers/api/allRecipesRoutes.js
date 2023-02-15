836ec6e599716a09697458da031d84c1dcd2ca6f
Matthew Walker <matthewevwalker@gmail.com>
2023-02-14 19:40:52 -0600

    Cards are now appearing with seed data on allrecipesRoutes

--- controllers/api/allRecipesRoutes.js
+++ controllers/api/allRecipesRoutes.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
+const { Recipe } = require('../../models')
 
 
+router.get('/', async (req, res) => {
+  try {
+    const recipeData = await Recipe.findAll();
+    
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
 
-
-router.get('/', (req, res) => {
-    res.render('allrecipes', {layout: 'main'})
+    res.render('allrecipes', { recipes })
+  } catch (err) {
+    res.status(500).json(err);
+  }
   })
+
+
 
 // router.get('/api/recipe', (req, res) => {
 //     res.sendFile('/js/index.html')
